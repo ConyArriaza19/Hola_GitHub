55046ff34dca3dad371ed34fba0165b30e2486f9
ConyArriaza19 <co.arriaza@duocuc.cl>
2025-06-16 22:40:30 -0400

    Agregando nuevas funciones al repositorio

---
+++ promedio.py
@@ -0,0 +1,13 @@
+# promedio.py
+
+print("\n=== FUNCIÓN DE PROMEDIO ===")
+
+# Lista de números:
+
+Numeros = [10, 20, 30, 40, 50]
+
+# Promedio con funciones:
+
+promedio = sum(Numeros) / len(Numeros)
+
+print("\nEl promedio de los números es: ", promedio)
